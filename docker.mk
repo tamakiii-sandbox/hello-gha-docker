95d2f9ac3aa08d1ac5edc73d58dbb472f433deab
tamakiii <tamakiii@users.noreply.github.com>
2023-03-11 01:51:55 +0900

    fix

--- docker.mk
+++ docker.mk
@@ -1,9 +1,9 @@
 .PHONY: help build bash test clean
 
-ORGANIZATION := tamakiii-sandbox
-REPOSITORY := hello-gha-docker
-IMAGE := $(ORGANIZATION)/$(REPOSITORY)
-TAG := latest
+export ORGANIZATION := tamakiii-sandbox
+export REPOSITORY := hello-gha-docker
+export IMAGE := $(ORGANIZATION)/$(REPOSITORY)
+export TAG := latest
 
 help:
 	@cat $(firstword $(MAKEFILE_LIST))
